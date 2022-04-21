4763b63d1a56a6fb3eaaaed84e4dea0d52052583
Uri Frazier <uri@fourkitchens.com>
2022-04-21 17:38:41 -0500

    progress on attacks and special attk

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+function randomAttackGen(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;    
+}
+
 const app = Vue.createApp({
     data() {
         return {
+            round: 0,
             monsterMaxHP: 150,
             monsterHP: 150,
+            playerMaxHP: 100,
             playerHP: 100
         };
     },
@@ -11,11 +17,29 @@ const app = Vue.createApp({
             return {
                 width: (this.monsterHP / this.monsterMaxHP * 100) + "%"
             }
+        },
+        playerBarStyles() {
+            return {
+                width: (this.playerHP / this.playerMaxHP * 100) + "%"
+            }
+        },
+        specialAtk() {
+            return !(this.round % 3 === 0);
         }
     },
     methods: {
         attackMonster() {
-            this.monsterHP -= 5;
+            this.monsterHP -= randomAttackGen(7, 15);
+            this.attackPlayer();
+            this.round++;
+        },
+        specialAttackMonster() {
+            this.monsterHP -= randomAttackGen(8, 20);
+            this.attackPlayer();
+            this.round++;
+        },        
+        attackPlayer() {
+            this.playerHP -= randomAttackGen(5, 12);
         }
     }
 });
