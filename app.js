80aed7dd6f43b2fa4e4fc37fb83f1c791b3bfe64
Uri Frazier <uri@fourkitchens.com>
2022-07-13 20:15:35 -0700

    More refining of game and attack logic

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function randomAttackGen(min, max) {
+function getRandomNum(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;    
 }
 
@@ -11,7 +11,9 @@ const app = Vue.createApp({
             monsterMaxHP: 150,
             monsterHP: 150,
             playerMaxHP: 100,
-            playerHP: 100
+            playerHP: 100,
+            winner: null,
+            active_game: true,
         };
     },
     computed: {
@@ -32,29 +34,61 @@ const app = Vue.createApp({
             }
             // if user doesn't have special available, then send 'true' value to 'disabled' attribute.
             return !this.special == true;
+        },
+        activeGame() {
+            if (this.winner) {
+                return !this.active_game == false;
+            }
+        }
+    },
+    watch: {
+        playerHP(value) {
+            if (value <= 0 && this.monsterHP <= 0) {
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                this.playerHP = 0; // Set to zero as negative CSS percentages don't work
+                this.winner = 'monster';
+            }
+        },
+        monsterHP(value) {
+            if (value <=0 && this.playerHP <= 0) {
+                this.winner = 'draw';
+            } else if (value <=0) {
+                this.monsterHP = 0; // Set to zero as negative CSS percentages don't work
+                this.winner = 'player';
+            } 
+        },
+        winner(value) {
+            if (this.winner) {
+                this.special = false;
+            }
         }
     },
     methods: {
         attackMonster() {
-            this.monsterHP -= randomAttackGen(7, 15);
-            if (this.monsterHP < 0 ) {
-                this.monsterHP = 0;
-            }            
+            this.monsterHP -= getRandomNum(7, 15);         
             this.attackPlayer();
             this.round++;
             this.limit_break++;
         },
         specialAttackMonster() {
-            this.monsterHP -= randomAttackGen(8, 20);
+            this.monsterHP -= getRandomNum(8, 20);
             this.attackPlayer();
             this.special = 0;
             this.limit_break = 0;
         },        
         attackPlayer() {
-            this.playerHP -= randomAttackGen(5, 12);
-            if (this.playerHP < 0 ) {
-                this.playerHP = 0;
-            }            
+            this.playerHP -= getRandomNum(5, 12);     
+        },
+        healPlayer() {
+            const healValue = getRandomNum(8, 20);
+            if (this.playerHP + healValue > 100) {
+                this.playerHP = 100;
+            } else {
+                this.playerHP += healValue;
+            }
+            this.attackPlayer();
+            this.round++;         
         }
     }
 });
